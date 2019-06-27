83abff2547b1564c55684d96c91f51032e1bf9d2
Arthur Cahen <arthur.cahen@gmail.com>
2019-06-27 11:11:09 +0200

    Update cleaning script

--- pipeline_move-and-scrape.sh
+++ pipeline_move-and-scrape.sh
@@ -1,5 +1,8 @@
-mv -v ~/Downloads/*.mp3 ~/Music/raw/ || true;
+mv -v ~/Downloads/*.mp3 ~/Music/music/raw/ || true;
 rename -vS 'www.my-free-mp3.net' '' *;
+rename -vS 'my-free-mp3s' '' *;
+rename -vS '(Original Mix)' '' *;
+rename -vS '(Club Mix)' '' *;
 rename -vS '[Video]' '' *;
 rename -vS '.com' '' *;
 rename -vS 'www.' '' *;
