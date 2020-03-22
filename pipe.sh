f601f34b351828043aacd4de148f61e97ee813c7
Arthur Cahen <arthurcahen@MacBook-Pro-de-Arthur.home>
2020-03-22 11:47:42 +0100

    Fix path bug, rename

--- pipe.sh
+++ pipe.sh
@@ -1,5 +1,7 @@
-echo "Move & Clean - GO!";
+echo "Move & Clean - GO! \n********************\n\n";
+
 mv -v ~/Downloads/*.mp3 ~/Music/music/raw/ || true;
+cd "~/Music/Music/raw"
 rename -vS 'www.my-free-mp3.net' '' *;
 rename -vS 'my-free-mp3s' '' *;
 rename -vS '(Original Mix)' '' *;
@@ -12,4 +14,8 @@ rename -vS '  ' ' ' *;
 rename -vS ' .mp3' '.mp3' *;
 rename -vS ' .mp3' '.mp3' *;
 rename -vS ' .mp3' '.mp3' *;
+
+echo "\n\nDuplicates:"
 ls *"(1).mp3";
+
+echo "\n\n********************\nEnd of move-and-scrape"
